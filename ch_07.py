87e6c9995d54b9efa3cdda185f2ab624f6404ab6
komal-Chaudhary-09 <komalchoudhary260408@gmail.com>
2026-05-24 12:02:41 +0530

    loops

--- ch_07.py
+++ ch_07.py
@@ -66,3 +66,75 @@ reverse =""
 for ch in text:
     reverse = ch+reverse
 print(reverse)
+
+
+# practice questions
+
+for i in range(1,20):
+    print(i)
+
+for i in range (1,10,2):
+    print(i)
+n = 5
+fact = 1
+
+for i in range (1,n+1):
+    fact = fact*i
+    print(fact)
+    
+text = " hello i am komal"
+reverse =""
+
+for ch in text:
+    reverse = ch + reverse
+print (reverse)
+    
+for i in range (1,6):
+    print("*"*i)
+
+list  =[1,4,2,5,7,98]
+large = list[0]
+
+for i in list :
+    if i > large:
+        large = i
+print(large)
+
+l = ["haary" ,"sahil","sachin","komal"]
+for name in l:
+    if(name.startswith("s")):
+        print(f"hello {name}")
+
+n = 5
+i= 1
+while(i<11):
+    print(n*i)
+    i+=1
+
+n = 3
+
+for i in range (2,n) :
+     if(n%i==0):
+      print("not prime")
+      break;
+    
+else:
+        print("prime")
+n= 5
+i=1
+sum = 0
+while (i<=n):
+    sum +=i
+    i +=1
+print(sum)
+
+n = int(input("enter the number"))
+for i in range (1,n+1):
+    if(i==1 or i==n):
+        print("*" * n, end ="")
+    else:
+        print("*", end="")
+        print(" "* (n-2), end="")
+        print("*", end ="")
+    print("")
+    
